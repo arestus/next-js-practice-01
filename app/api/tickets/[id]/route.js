1a44f9d61e60260fb7a646a6a47a2af0f237f178
arestus <arestus@gmail.com>
2024-05-27 17:45:30 +0200

    a ticket route

---
+++ app/api/tickets/[id]/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export async function DELETE(_, { params }) {
+  const id = params.id;
+
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const { error } = await supabase.from("Tickets").delete().eq("id", id);
+
+  return NextResponse.json({ error });
+}
