b7f5b316e7fbe5c87e0e0b3afcb4a6ba250401dc
arestus <arestus@gmail.com>
2024-05-28 22:29:26 +0200

    loading page

---
+++ app/(dashboard)/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+      <h2 className="text-primary">Loading...</h2>
+      <p>This may take a few seconds.</p>
+    </main>
+  );
+}
